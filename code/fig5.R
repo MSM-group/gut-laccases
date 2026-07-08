46579b4e525787315e2be1fdbb0f370930966563
schaermi <milo.schaerer@eawag.ch>
2026-07-08 18:58:55 +0200

    fig5 legend fix

--- code/fig5.R
+++ code/fig5.R
@@ -39,7 +39,8 @@ laccase_biotransformation_plot <- ggplot2::ggplot(dat, aes(x = mean, y = compoun
                 position=position_dodge(.9)) +
   theme_pubr() +
   scale_fill_discrete(palette = pal,
-                      breaks = c("MCO1 w/ ABTS", "MCO1 w/o ABTS", "aMCO w/ ABTS", "aMCO w/o ABTS", "mMCO w/ ABTS", "mMCO w/o ABTS", "Vei1 w/ ABTS", "Vei1 w/o ABTS", "Abiotic control w/ ABTS")) +
+                      breaks = c("MCO1 w/ ABTS", "MCO1 w/o ABTS", "aMCO w/ ABTS", "aMCO w/o ABTS", "mMCO w/ ABTS", "mMCO w/o ABTS", "Vei1 w/ ABTS", "Vei1 w/o ABTS", "Abiotic control w/ ABTS"),
+                      guide = guide_legend(reverse = TRUE)) +
   labs(y = "Compound", x = expression(C/C[0])) +
   theme(legend.text = element_text(size = 8, margin = margin(l = 1.5, unit = "pt")),
         legend.title = element_blank(),
